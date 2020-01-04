bc33eca9926a272c7314b15f3a7a5875fc038261
mounibec <mounibec@gmail.com>
2020-01-04 21:55:05 +0100

    Cleaning up

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-import {Web3ReactProvider, useWeb3React} from "@web3-react/core";
-import {Web3Provider} from "@ethersproject/providers";
-
 import 'semantic-ui-css/semantic.min.css';
 import "./stylesheets/style.scss";
 import 'c3/c3.css';
@@ -12,24 +9,12 @@ import Swapper from "./swapper";
 
 declare let document: any;
 
-function getLibrary(provider: any) {
-  return new Web3Provider(provider);
-}
-
-const App = () => {
-  return (
-    <Swapper/>
-  )
-};
-
 class Root extends React.Component {
 
   render() {
     return (
       <div className={"root-container"}>
-        <Web3ReactProvider getLibrary={getLibrary}>
-          <App/>
-        </Web3ReactProvider>
+        <Swapper/>
       </div>
     )
   }
